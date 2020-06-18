fa86512e2bd96ca1681e31c59e1d6cc3722fd915
karol <karol50709@gmail.com>
2020-06-18 17:47:22 +0200

    zmiana statusu issue

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('project/<project_id>', views.project_detail),
     path('add-issue', views.issueAdd, name='issue-add'),
     path('issue/<issue_id>', views.issue_detail, name='issue'),
+    path('issue-update/<issue_id>/<issue_position>/', views.issue_update, name='issue-update'),
 
 ]
