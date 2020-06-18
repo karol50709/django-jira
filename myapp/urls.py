036b98e9c61ab5ebb6e7ddf74c38ea7bb194b9e5
karol <karol50709@gmail.com>
2020-06-18 16:10:30 +0200

    dodany widok issue + js poprawa linkow

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.init),
     path('index', views.index),
-    path('all-projects', views.allProjects),
+    path('all-projects', views.allProjects, name='all-projects'),
     path('add-project', views.projectAdd, name='project-add'),
     path('project/<project_id>', views.project_detail),
-    path('add-issue', views.issueAdd, name='issue-add')
+    path('add-issue', views.issueAdd, name='issue-add'),
+    path('issue/<issue_id>', views.issue_detail, name='issue'),
 
 ]
